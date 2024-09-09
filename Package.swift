24de34cc0f21139e77587edf45f6b5655fbaa4e5
Adam Fowler <adamfowler71@gmail.com>
2024-09-09 13:43:24 +0100

    Update to user HB RC 6

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["HummingbirdDocs"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-rc.2"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-rc.6"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-auth.git", from: "2.0.0-rc.2"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-compression.git", from: "2.0.0-alpha.2"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-fluent.git", from: "2.0.0-beta.2"),
