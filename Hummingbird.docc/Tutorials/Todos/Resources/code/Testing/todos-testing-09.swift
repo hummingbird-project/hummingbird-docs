181a7e32d80dd1c5fe7be4a7a0c581413468db13
Ron Magno <ron.zohan@gmail.com>
2024-10-25 23:31:48 +1300

    Update tutorial for Swift 6 (#63)
    * Update documentation for Swift 6
    
    * Update other parts of the testing tutorial

--- Hummingbird.docc/Tutorials/Todos/Resources/code/Testing/todos-testing-09.swift
+++ Hummingbird.docc/Tutorials/Todos/Resources/code/Testing/todos-testing-09.swift
@@ -17,7 +17,8 @@ final class AppTests: XCTestCase {
         let title: String
         let order: Int?
     }
-    func create(title: String, order: Int? = nil, client: some TestClientProtocol) async throws -> Todo {
+
+    static func create(title: String, order: Int? = nil, client: some TestClientProtocol) async throws -> Todo {
         let request = CreateRequest(title: title, order: order)
         let buffer = try JSONEncoder().encodeAsByteBuffer(request, allocator: ByteBufferAllocator())
         return try await client.execute(uri: "/todos", method: .post, body: buffer) { response in
@@ -31,7 +32,7 @@ final class AppTests: XCTestCase {
     func testCreate() async throws {
         let app = try await buildApplication(TestArguments())
         try await app.test(.router) { client in
-            let todo = try await self.create(title: "My first todo", client: client)
+            let todo = try await Self.create(title: "My first todo", client: client)
             XCTAssertEqual(todo.title, "My first todo")
         }
     }
