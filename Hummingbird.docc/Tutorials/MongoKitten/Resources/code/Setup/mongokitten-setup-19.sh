db50c5fc32171e6a3a4c0fa94a8afdc307ecd544
Joannis Orlandos <joannis@orlandos.nl>
2024-11-27 12:05:57 +0100

    Use the same entity name

--- Hummingbird.docc/Tutorials/MongoKitten/Resources/code/Setup/mongokitten-setup-19.sh
+++ Hummingbird.docc/Tutorials/MongoKitten/Resources/code/Setup/mongokitten-setup-19.sh
@@ -5,4 +5,4 @@ Content-Length: 66
 Date: Thu, 21 Nov 2024 14:55:26 GMT
 Server: MeowSocial
 
-[{"_id":"6744f4e1c89cdc21285c6fd5","name":"Andromeda"}]
+[{"_id":"6744f4e1c89cdc21285c6fd5","name":"Milo"}]
